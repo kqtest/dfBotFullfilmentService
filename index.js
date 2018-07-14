931d493452817067392a287c804e443531bf18e4
Kevin Qian <yqian@expedia.com>
2018-07-13 22:51:19 -0700

    update

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+ }));
+ 
 app.get('/', function(req, res){
     res.send('Welcome to Virtual Agent Bot Fullfilment Service');
 });
